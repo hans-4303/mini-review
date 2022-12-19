652e17d78a0f1317e72734abcda969799ea26a20
Johnny_P <shinezl39@gmail.com>
2022-12-19 14:59:49 +0900

    redux ready

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-
 /* 웹을 사용할 때 파이어베이스를 들고 오기 위해 import */
 /* import { app } from './database/firebase'; */
 
@@ -14,12 +10,22 @@ js, css를 들고 올 때 파일 전체를 import 해오는 경우,
 전체 파일에 내용이 적용될 수 있다고?
 -> 아마 최상위인 App.js나 index.js라서 해당되는 것 아닐까 */
 import './database/firebase'
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './redux';
+
+const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/redux/index.js
@@ -0,0 +1,9 @@
+/* 리덕스 모듈을 하나로 묶고, 내보내는 index.js */
+
+import { combineReducers } from 'redux'
+
+const rootReducer = combineReducers({
+
+});
+
+export default rootReducer;

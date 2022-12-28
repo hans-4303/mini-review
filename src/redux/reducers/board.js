d38026892fd80f9e5f4a66161337514d9db97842
Johnny_P <shinezl39@gmail.com>
2022-12-28 14:36:27 +0900

    board의 like를 배열, ADD_LIKE_USER 리듀서 추가

--- src/redux/reducers/board.js
+++ src/redux/reducers/board.js
@@ -5,7 +5,8 @@ const initialState = [
     title: "첫번째 게시글",
     content: "첫번째 글입니다",
     view: 0,
-    like: 1,
+    /* 좋아요를 누른 사람을 배열로 받기 */
+    like: [],
   },
   {
     boardId: 2,
@@ -13,7 +14,7 @@ const initialState = [
     title: "두번째 게시글",
     content: "두번째 글입니다",
     view: 0,
-    like: 1,
+    like: [],
   },
 ];
 
@@ -33,8 +34,10 @@ function board(state = initialState, action) {
       배열의 개수는 바뀌지 않고, 값만 수정할 때 map을 사용해보기(배열의 길이만큼 반복되니까)
 
       수정할 id 값을 비교하고, board 값을 바꿔서 넣어준다. */
-      
-      const editBoard = state.map((board) => (board.boardId == action.payload.boardId ? action.payload : board))
+
+      const editBoard = state.map((board) =>
+        board.boardId == action.payload.boardId ? action.payload : board
+      );
       return editBoard;
     case "ADD_BOARD":
       /* 새로운 보드 값을 받아와서 boardId 값을 부여한 후에 추가하기
@@ -44,15 +47,38 @@ function board(state = initialState, action) {
         ...action.payload,
         boardId: boardId,
         view: 0,
-        like: 0
-      }
+        /* 새로운 게시글에서도 좋아요를 배열로 받기 */
+        like: [],
+      };
       boardId++;
       return state.concat(newBoard);
+    case "ADD_LIKE_USER":
+      /* board의 좋아요 버튼을 눌렀을 때 값 확인
+      1) userInfoList의 like는 boardId, title을 포함하는 배열
+      2) board의 like 값은 userEmail과 함께 다룰 배열 */
+      const newLikeAddress = action.payload.userEmail;
+      return state.map((board) =>
+        board.boardId == action.payload.boardId
+          ? {
+              ...board,
+              /* like 키와 값은 board.like에서 값을 찾고, 있다면 이전 값 없다면 추가한 값 넣기 */
+              like: board.like.find(
+                (boardLiked) => boardLiked == action.payload.userEmail
+              )
+                ? board.like
+                : board.like.concat(newLikeAddress),
+            }
+          : board
+      );
     case "UPDATE_VIEW":
       /* 보드 값을 받아오고, 해당 페이지 주소의 파라미터인 /:id와
       비교한 후에 해당 board의 id를 올리거나,
       조건이 맞지 않다면 내버려둔다. */
-      const viewBoard = state.map((board) => (board.boardId == action.payload ? {...board, view: board.view + 1} : board))
+      const viewBoard = state.map((board) =>
+        board.boardId == action.payload
+          ? { ...board, view: board.view + 1 }
+          : board
+      );
       return viewBoard;
     default:
       return state;
@@ -60,8 +86,11 @@ function board(state = initialState, action) {
 }
 
 export const deleteBoard = (id) => ({ type: "DELETE_BOARD", payload: id });
-export const modifyBoard = (board) => ({type: "MODIFY_BOARD", payload: board});
-export const addBoard = (board) => ({type: "ADD_BOARD", payload: board})
-export const viewBoard = (board) => ({type: "UPDATE_VIEW", payload: board})
+export const modifyBoard = (board) => ({
+  type: "MODIFY_BOARD",
+  payload: board,
+});
+export const addBoard = (board) => ({ type: "ADD_BOARD", payload: board });
+export const viewBoard = (board) => ({ type: "UPDATE_VIEW", payload: board });
 
 export default board;
